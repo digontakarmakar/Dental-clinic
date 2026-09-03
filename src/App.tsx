32ae37686df87b8c1de9ff282e4de43dcac0a681
digontakarmakar <digontakarmakar5@gmail.com>
2026-09-03 20:54:09 +0600

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from '@/components/ui/Toaster';
-import Layout from '@/components/layout/Layout';
-import Home from '@/pages/Home';
-import About from '@/pages/About';
-import Doctors from '@/pages/Doctors';
-import DoctorProfile from '@/pages/DoctorProfile';
-import Treatments from '@/pages/Treatments';
-import TreatmentDetail from '@/pages/TreatmentDetail';
-import Technology from '@/pages/Technology';
-import BeforeAfter from '@/pages/BeforeAfter';
-import PatientStories from '@/pages/PatientStories';
-import Contact from '@/pages/Contact';
-import BookAppointment from '@/pages/BookAppointment';
-import FAQ from '@/pages/FAQ';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/Toaster";
+import Layout from "@/components/layout/Layout";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Doctors from "@/pages/Doctors";
+import DoctorProfile from "@/pages/DoctorProfile";
+import Treatments from "@/pages/Treatments";
+import TreatmentDetail from "@/pages/TreatmentDetail";
+import Technology from "@/pages/Technology";
+import BeforeAfter from "@/pages/BeforeAfter";
+import PatientStories from "@/pages/PatientStories";
+import Contact from "@/pages/Contact";
+import BookAppointment from "@/pages/BookAppointment";
+import FAQ from "@/pages/FAQ";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Dental-clinic">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
